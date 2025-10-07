51e0d50f60690d96a95afcbc40b667c277733864
Michael <michael.elliston95@gmail.com>
2025-10-07 14:01:37 -0500

    scanner and prompt added

--- src/main/java/BedtimeStories.java
+++ src/main/java/BedtimeStories.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.io.FileReader;
+import java.util.Scanner;
 
 public class BedtimeStories {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        String selection;
+
+        System.out.println("Please enter the number corresponding to the story you'd like to read:\n\t1. Goldilocks\n\t2. Hansel and Gretel\n\t3. Mary Had a Little Lamb");
+        selection = myScanner.nextLine();
+    }
 }
+
